fa156bec4196a0f9dfa9b23171a66be76e2cc80e
Robin Röttger <robin.roettger@beemo.eu>
2019-05-16 11:56:16 +0200

    Export toolbox builder

--- projects/ngx-blockly/src/public_api.ts
+++ projects/ngx-blockly/src/public_api.ts
@@ -10,4 +10,5 @@ export * from './lib/ngx-blockly/models/custom-block';
 export * from './lib/ngx-blockly/models/xml-block';
 export * from './lib/ngx-blockly/models/category';
 export * from './lib/ngx-blockly/models/block-mutator';
+export * from './lib/ngx-blockly/services/ngx-toolbox-builder.service';
 export * from './lib/ngx-blockly.module';
