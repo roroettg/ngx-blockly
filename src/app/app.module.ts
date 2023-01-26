b7864f7991fba152a3d28d5a8af96364f9ccfe44
Robin Roettger <robin.roettger@andsafe.de>
2023-01-26 09:54:57 +0100

    Release 14.0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgxBlocklyModule } from '../../projects/ngx-blockly/src/lib/ngx-blockly.module';
+import { NgxBlocklyModule } from 'ngx-blockly';
 
 
 import 'blockly/blocks';
